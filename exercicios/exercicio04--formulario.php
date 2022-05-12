68790e488d298d1b9dca9e7f1ba2f17b18950269
misael2022 <ggmisajvrmichel@gmail.com>
2022-05-12 16:10:15 -0300

    ettetett

--- exercicios/exercicio04--formulario.php
+++ exercicios/exercicio04--formulario.php
@@ -7,6 +7,54 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>ola</h1>
+    <hr>
+
+<?php
+if( empty($_POST["nome"]) || empty($_POST["email"]) ){
+    echo "<p>Por favor preencha nome e email!</p>";
+?>
+    <p style="color: brown;">Por favor preencha nome e email</p>
+    <p><a href="exercicio04-formulario.html">Voltar</a></p> <!-- opcional -->
+<?php
+}else {
+
+// Captura os dados enviados a partir do formulario
+    /* echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>";  */
     
+    // Capturando os dados enviados a partir do formulario
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $idade = $_POST["idade"];
+    // ?? operador de coalescencia nula
+    $interesses = $_POST["interesses"] ?? [];
+    $informativos = $_POST["informativos"]; 
+    $mensagem = $_POST["mensagem"];
+?>
+
+    <h2>Dados:</h2>
+    <ul>
+        <li> Nome: <?=$nome?> </li>
+        <li> Email: <?=$email?> </li>
+        <li> Idade: <?=$idade?> </li>
+
+        <!-- Aqui usamos o operador de navegação (!) para inverter a logica da função empty -->
+        <!--  Se intereses NÃO ESTIVER VAZIO -->
+        <?php if( !empty($interesses)){ ?>
+            <!-- entao, faca tudo abaixo -->
+        <li> Interesses: 
+            <ul>
+            <?php foreach($interesses as $interesse){?>
+            <li> <?=$interesse?></li>
+            <?php } ?>
+            </ul>
+       </li>
+       <?php } ?>
+        <li> informativos: <?=$informativos?> </li>
+        <li> Mensagem: <?=$mensagem?> </li>
+    </ul>
+    <?php }?>
 </body>
 </html>
